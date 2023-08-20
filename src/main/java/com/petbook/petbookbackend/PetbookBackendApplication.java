8d83d4fe1735d8f89634e5b4009d5085848108d7
diptomistry <diptomistrybd@gmail.com>
2023-08-21 02:04:03 +0600

    Initial commit

---
+++ src/main/java/com/petbook/petbookbackend/PetbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petbook.petbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetbookBackendApplication.class, args);
+	}
+
+}
